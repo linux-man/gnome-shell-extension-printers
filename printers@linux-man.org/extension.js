ed7e41ec2ab0baf43dcff7a4bbcc2fac20fae050
João Lopes <linux-man@hotmail.com>
2022-02-09 03:51:02 +0000

    Code converted to async/await methods.

--- printers@linux-man.org/extension.js
+++ printers@linux-man.org/extension.js
@@ -19,18 +19,20 @@ const errorIcon = 'printer-error-symbolic';
 
 let _timeout = null;
 
-function spawn_async(args, callback) {
-    let [success, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null, args, null, GLib.SpawnFlags.SEARCH_PATH, null);// | GLib.SpawnFlags.DO_NOT_REAP_CHILD
-    let strOUT = '';
-    if(success) {
-        let out_reader = new Gio.DataInputStream({base_stream: new Gio.UnixInputStream({fd: out_fd})});
-        let [out, size] = out_reader.read_line(null);
-        while (out !== null) {
-            strOUT += ByteArray.toString(out) + '\n';
-            [out, size] = out_reader.read_line(null);
+function spawn_async(args) {
+    return new Promise((resolve, reject) => {
+        let [success, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null, args, null, GLib.SpawnFlags.SEARCH_PATH, null);// | GLib.SpawnFlags.DO_NOT_REAP_CHILD
+        let strOUT = '';
+        if(success) {
+            let out_reader = new Gio.DataInputStream({base_stream: new Gio.UnixInputStream({fd: out_fd})});
+            let [out, size] = out_reader.read_line(null);
+            while (out !== null) {
+                strOUT += ByteArray.toString(out) + '\n';
+                [out, size] = out_reader.read_line(null);
+            }
         }
-    }
-    callback(strOUT);
+        resolve(strOUT);
+    })
 }
 
 function PopupIconMenuItem(icon, label) {
@@ -42,8 +44,7 @@ function PopupIconMenuItem(icon, label) {
     return _item;
 }
 
-const PrintersManager = GObject.registerClass(
-class PrintersManager extends PanelMenu.Button {
+const PrintersManager = GObject.registerClass(class PrintersManager extends PanelMenu.Button {
 
     _init() {
         super._init(null, 'PrintersManager')
@@ -91,7 +92,7 @@ class PrintersManager extends PanelMenu.Button {
         Util.spawn(['lp', '-i', item.job, '-q 100']);
     }
 
-    refresh() {
+    async refresh() {
         this.connect_to = this._settings.get_enum('connect-to');
         this.show_icon = this._settings.get_enum('show-icon');
         this.show_error = this._settings.get_boolean('show-error');
@@ -104,87 +105,83 @@ class PrintersManager extends PanelMenu.Button {
         printers.connect('activate', this.onShowPrintersClicked.bind(this));
         this.menu.addMenuItem(printers);
 //Add Printers
-        spawn_async(['/usr/bin/lpstat', '-a'], (out) => {
-            this.printers = [];
-            spawn_async(['/usr/bin/lpstat', '-d'], (out2) => {//To check default printer
-                if(out2.split(': ')[1] != undefined) out2 = out2.split(': ')[1].trim();
-                else out2 = 'no default';
-                out = out.split('\n');
-                this.printersCount = out.length - 1;
-                if(this.printersCount > 0) {
-                    if(this.connect_to == 1) this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-                    for(let n = 0; n < this.printersCount; n++) {
-                        let printer = out[n].split(' ')[0];
-                        this.printers.push(printer);
-                        if(this.connect_to == 1) {
-                            let printerItem = PopupIconMenuItem('emblem-documents-symbolic', printer);
-                            if(out2.toString() == printer.toString()) printerItem.add(new St.Icon({ icon_name: 'emblem-default-symbolic', style_class: 'popup-menu-icon' }));
-                            printerItem.printer = printer;
-                            printerItem.connect('activate', this.onShowJobsClicked.bind(this));
-                            this.menu.addMenuItem(printerItem);
-                        }
-                    }
+        this.printers = [];
+        let p_list = await spawn_async(['/usr/bin/lpstat', '-a']);
+        let p_default = await spawn_async(['/usr/bin/lpstat', '-d']);
+        if(p_default.split(': ')[1] != undefined) p_default = p_default.split(': ')[1].trim();
+        else p_default = 'no default';
+        p_list = p_list.split('\n');
+        this.printersCount = p_list.length - 1;
+        if(this.printersCount > 0) {
+            if(this.connect_to == 1) this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            for(let n = 0; n < this.printersCount; n++) {
+                let printer = p_list[n].split(' ')[0];
+                this.printers.push(printer);
+                if(this.connect_to == 1) {
+                    let printerItem = PopupIconMenuItem('emblem-documents-symbolic', printer);
+                    if(p_default.toString() == printer.toString()) printerItem.add(new St.Icon({ icon_name: 'emblem-default-symbolic', style_class: 'popup-menu-icon' }));
+                    printerItem.printer = printer;
+                    printerItem.connect('activate', this.onShowJobsClicked.bind(this));
+                    this.menu.addMenuItem(printerItem);
                 }
-//Add Jobs
-                spawn_async(['/usr/bin/lpstat', '-o'], (out) => {
+            }
+        }
+//Jobs
+        let p_jobs = await spawn_async(['/usr/bin/lpstat', '-o']);
 //Cancel all Jobs
-                    if(out.length > 0) {
-                        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-                        let cancelAll = PopupIconMenuItem('edit-delete-symbolic', _('Cancel all jobs'));
-                        cancelAll.connect('activate', this.onCancelAllJobsClicked.bind(this));
-                        this.menu.addMenuItem(cancelAll);
-                    }
-                    out = out.split(/\n/);
-                    this.jobsCount = out.length - 1
-                    spawn_async(['/usr/bin/lpq', '-a'], (out2) => {
-                        out2 = out2.replace(/\n/g, ' ').split(/\s+/);
-                        let sendJobs = [];
-                        for(var n = 0; n < out.length - 1; n++) {
-                            let line = out[n].split(' ')[0].split('-');
-                            let job = line.slice(-1)[0];
-                            let printer = line.slice(0, -1).join('-');
-                            let doc = out2[out2.indexOf(job) + 1];
-                            for(var m = out2.indexOf(job) + 2; m < out2.length - 1; m++) {
-                                if(isNaN(out2[m]) || out2[m + 1] != 'bytes') doc = doc + ' ' + out2[m];
-                                else break;
-                            }
-                            if(doc.length > 30) doc = doc + '...';
-                            let text = doc;
-                            if(this.job_number) text += ' (' + job + ')';
-                            text += ' ' + _('at') + ' ' + printer;
-                            let jobItem = PopupIconMenuItem('edit-delete-symbolic', text);
-                            if(out2[out2.indexOf(job) - 2] == 'active') jobItem.add(new St.Icon({ icon_name: 'emblem-default-symbolic', style_class: 'popup-menu-icon' }));
-                            jobItem.job = job;
-                            jobItem.connect('activate', this.onCancelJobClicked.bind(jobItem));
-                            this.menu.addMenuItem(jobItem);
-                            if(this.send_to_front && out2[out2.indexOf(job) - 2] != 'active' && out2[out2.indexOf(job) - 2] != '1st') {
-                                sendJobs.push(PopupIconMenuItem('go-up-symbolic', text));
-                                sendJobs[sendJobs.length - 1].job = job;
-                                sendJobs[sendJobs.length - 1].connect('activate', this.onSendToFrontClicked.bind(sendJobs[sendJobs.length - 1]));
-                            }
-                        }
+        if(p_jobs.length > 0) {
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            let cancelAll = PopupIconMenuItem('edit-delete-symbolic', _('Cancel all jobs'));
+            cancelAll.connect('activate', this.onCancelAllJobsClicked.bind(this));
+            this.menu.addMenuItem(cancelAll);
+        }
+//Add Jobs
+        p_jobs = p_jobs.split(/\n/);
+        this.jobsCount = p_jobs.length - 1
+        let p_jobs2 = await spawn_async(['/usr/bin/lpq', '-a']);
+        p_jobs2 = p_jobs2.replace(/\n/g, ' ').split(/\s+/);
+        let sendJobs = [];
+        for(var n = 0; n < p_jobs.length - 1; n++) {
+            let line = p_jobs[n].split(' ')[0].split('-');
+            let job = line.slice(-1)[0];
+            let printer = line.slice(0, -1).join('-');
+            let doc = p_jobs2[p_jobs2.indexOf(job) + 1];
+            for(var m = p_jobs2.indexOf(job) + 2; m < p_jobs2.length - 1; m++) {
+                if(isNaN(p_jobs2[m]) || p_jobs2[m + 1] != 'bytes') doc = doc + ' ' + p_jobs2[m];
+                else break;
+            }
+            if(doc.length > 30) doc = doc + '...';
+            let text = doc;
+            if(this.job_number) text += ' (' + job + ')';
+            text += ' ' + _('at') + ' ' + printer;
+            let jobItem = PopupIconMenuItem('edit-delete-symbolic', text);
+            if(p_jobs2[p_jobs2.indexOf(job) - 2] == 'active') jobItem.add(new St.Icon({ icon_name: 'emblem-default-symbolic', style_class: 'popup-menu-icon' }));
+            jobItem.job = job;
+            jobItem.connect('activate', this.onCancelJobClicked.bind(jobItem));
+            this.menu.addMenuItem(jobItem);
+            if(this.send_to_front && p_jobs2[p_jobs2.indexOf(job) - 2] != 'active' && p_jobs2[p_jobs2.indexOf(job) - 2] != '1st') {
+                sendJobs.push(PopupIconMenuItem('go-up-symbolic', text));
+                sendJobs[sendJobs.length - 1].job = job;
+                sendJobs[sendJobs.length - 1].connect('activate', this.onSendToFrontClicked.bind(sendJobs[sendJobs.length - 1]));
+            }
+        }
 //Send to Front
-                        if(this.send_to_front && sendJobs.length > 0) {
-                            let subMenu = new PopupMenu.PopupSubMenuMenuItem(_('Send to front'));
-                            for(let n = 0; n < sendJobs.length; n++) subMenu.menu.addMenuItem(sendJobs[n]);
-                            this.menu.addMenuItem(subMenu);
-                        }
+        if(this.send_to_front && sendJobs.length > 0) {
+            let subMenu = new PopupMenu.PopupSubMenuMenuItem(_('Send to front'));
+            for(let n = 0; n < sendJobs.length; n++) subMenu.menu.addMenuItem(sendJobs[n]);
+            this.menu.addMenuItem(subMenu);
+        }
 //Update Icon
-                        if(this.jobsCount > 0 && this.show_jobs) this._jobs.text = this.jobsCount.toString();
-                        else this._jobs.text = '';
-                        this.show();
-                        if(this.show_icon == 0 || (this.show_icon == 1 && this.printersCount > 0) || (this.show_icon == 2 && this.jobsCount > 0)) this.show();
-                        else this.hide();
-                        spawn_async(['/usr/bin/lpstat', '-l'], (out) => {
-                            this.printError = out.indexOf('Unable') >= 0 || out.indexOf(' not ') >= 0 || out.indexOf(' failed') >= 0;
-                            if(this.printWarning) this._icon.icon_name = warningIcon;
-                            else if(this.show_error && this.printError) this._icon.icon_name = errorIcon;
-                            else this._icon.icon_name = printerIcon;
-                        });
-                    });
-                });
-            });
-        });
+        if(this.jobsCount > 0 && this.show_jobs) this._jobs.text = this.jobsCount.toString();
+        else this._jobs.text = '';
+        this.show();
+        if(this.show_icon == 0 || (this.show_icon == 1 && this.printersCount > 0) || (this.show_icon == 2 && this.jobsCount > 0)) this.show();
+        else this.hide();
+        let p_error = await spawn_async(['/usr/bin/lpstat', '-l']);
+        this.printError = p_error.indexOf('Unable') >= 0 || p_error.indexOf(' not ') >= 0 || p_error.indexOf(' failed') >= 0;
+        if(this.printWarning) this._icon.icon_name = warningIcon;
+        else if(this.show_error && this.printError) this._icon.icon_name = errorIcon;
+        else this._icon.icon_name = printerIcon;
     }
 
     onCupsSignal() {
